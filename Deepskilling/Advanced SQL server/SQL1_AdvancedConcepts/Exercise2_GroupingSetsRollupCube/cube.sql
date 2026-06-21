bf7f55b4988149aca922b961f7c695511a012be0
Akanksha Saraf <146836060+akankshasaraf18@users.noreply.github.com>
2026-06-21 15:55:48 +0530

    Add SQL query to include Customers join

--- Deepskilling/Advanced SQL server/SQL1_AdvancedConcepts/Exercise2_GroupingSetsRollupCube/cube.sql
+++ Deepskilling/Advanced SQL server/SQL1_AdvancedConcepts/Exercise2_GroupingSetsRollupCube/cube.sql
@@ -2,11 +2,11 @@ SELECT
     c.Region,
     p.Category,
     SUM(od.Quantity) AS TotalQuantity
-FROM Orders o
+FROM Customers c
+JOIN Orders o
+    ON c.CustomerID = o.CustomerID
 JOIN OrderDetails od
-ON o.OrderID = od.OrderID
-JOIN Customers c
-ON o.CustomerID = c.CustomerID
+    ON o.OrderID = od.OrderID
 JOIN Products p
-ON od.ProductID = p.ProductID
+    ON od.ProductID = p.ProductID
 GROUP BY CUBE(c.Region, p.Category);
