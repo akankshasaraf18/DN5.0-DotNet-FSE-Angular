8c98ac02886e49cf708b3d549c7532ab34eca20d
Akanksha Saraf <146836060+akankshasaraf18@users.noreply.github.com>
2026-07-01 17:46:19 +0530

    Add files via upload

---
+++ Deepskilling/WebApi/1.WebApi_Handson/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[]
+            {
+                "Apple",
+                "Banana",
+                "Orange"
+            });
+        }
+
+        // GET: api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"ID = {id}");
+        }
+
+        // POST: api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok($"Added : {value}");
+        }
+
+        // PUT: api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok($"Updated ID = {id} with Value = {value}");
+        }
+
+        // DELETE: api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleted ID = {id}");
+        }
+    }
+}
