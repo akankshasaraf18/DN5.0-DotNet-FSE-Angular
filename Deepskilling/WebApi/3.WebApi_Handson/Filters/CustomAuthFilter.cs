e28507a81ecf027b6bb23eb3813aeb3e154c0be4
Akanksha Saraf <146836060+akankshasaraf18@users.noreply.github.com>
2026-07-02 13:17:07 +0530

    Add files via upload

---
+++ Deepskilling/WebApi/3.WebApi_Handson/Filters/CustomAuthFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeWebApi.Filters
+{
+    public class CustomAuthFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.HttpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Invalid request - No Auth token"
+                };
+
+                return;
+            }
+
+            string token = context.HttpContext.Request.Headers["Authorization"];
+
+            if (!token.StartsWith("Bearer "))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Invalid request - Bearer token missing"
+                };
+
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ Deepskilling/WebApi/3.WebApi_Handson/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeWebApi.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            string path = @"C:\Temp\ExceptionLog.txt";
+
+            Directory.CreateDirectory(@"C:\Temp");
+
+            File.AppendAllText(
+                path,
+                DateTime.Now + " : " +
+                context.Exception.Message +
+                Environment.NewLine
+            );
+
+            context.Result = new ObjectResult("Internal Server Error")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
