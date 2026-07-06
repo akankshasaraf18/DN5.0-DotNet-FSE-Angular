0f34d59c3eb19696a0958b698f35942a19850b66
Akanksha Saraf <146836060+akankshasaraf18@users.noreply.github.com>
2026-07-07 00:31:12 +0530

    Add files via upload

---
+++ Deepskilling/WebApi/5.WebApi_Handson/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using EmployeeWebApi.Filters;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string securityKey = "mysuperdupersecretkey123456789012";
+
+var symmetricSecurityKey =
+    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters =
+        new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "mySystem",
+            ValidAudience = "myUsers",
+
+            IssuerSigningKey = symmetricSecurityKey
+        };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
